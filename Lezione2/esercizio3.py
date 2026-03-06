cfcef5df73c8b89bec48c88f44d8caee24fa2bf5
SheverinM <majid@pc.home>
2026-03-06 10:59:08 +0100

    ho fatto if-else

--- Lezione2/esercizio3.py
+++ Lezione2/esercizio3.py
@@ -1,3 +1,9 @@
 #Scrivere un programma che verifica se un numero inserito dall’utente è pari o dispari.
 
-numero = int(input("Inserisci un numero. Ti dico se è pari o dispari."))
+numero = int(input("Inserisci un numero. Ti dico se è pari o dispari: "))
+
+if numero%2 == 0:
+    print(f"Il {numero} è pari")
+
+else:
+    print(f"Il numero {numero} è dispari")
