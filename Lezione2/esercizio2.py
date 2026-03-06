c8026ed0a502e36b57b583254f77f07bed8e6ce4
SheverinM <majid@pc.home>
2026-03-06 13:27:27 +0100

    aggiunto le funzioni quadrato e cubo

--- Lezione2/esercizio2.py
+++ Lezione2/esercizio2.py
@@ -1,12 +1,21 @@
 #Scrivere un programma che chiede all’utente un numero intero e 
 #stampa il suo quadrato e il suo cubo
 
+def quadrato (n):
+    
+    return n**2
+
+def cubo (n):
+    
+    return n**3
+
+
 numero = int(input("inserisci un numero intero e ti restituisco"
                    " il suo quadrato e il suo cubo:"))
 
-quadrato = numero**2
-cubo = numero**3
 
-print(f"Il quadrato del numreo {numero} = {quadrato}",
-      f"Il cubo del numero {numero} = {cubo}", sep="\n")
+print(f"Il quadrato del numero \"{numero}\" è {quadrato(numero)}")
+      
+print(f"Il quadrato del numero \"{numero}\" è {cubo(numero)}") 
+
 
