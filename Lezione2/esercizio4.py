9ceb1b3d1db8d2afd27b0245bcb9abb41c3b5bbc
SheverinM <majid@pc.home>
2026-03-06 11:26:36 +0100

    ho completato esercizio

---
+++ Lezione2/esercizio4.py
@@ -0,0 +1,25 @@
+#Definire una funzione che prende come argomento una parola e una lettera 
+# e ritorna quante volte quella lettera è contenuta nella parola.
+
+def conta_ripetizione_lettera(parola, lettera):
+
+    contatore = 0
+
+    for c in parola:
+        if c == lettera:
+            contatore += 1
+
+    return contatore
+
+print("Inserisci una parola e una lettera e ti dico quante volte quella lettera",
+      "è contenuta nella parola. cominciamo:")
+
+parola = input("Inserisci una parola:")
+
+lettera = input("Inserisci una lettera:")
+
+ripetizioni = conta_ripetizione_lettera(parola, lettera)
+
+print(f"La lettera \"{lettera}\" è stata ripetuta {ripetizioni} volte nella parola \"{parola}\"")
+
+
