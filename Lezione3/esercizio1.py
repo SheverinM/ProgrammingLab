2ea97ae484115da05583476ab2df02119853513d
SheverinM <majid@pc.home>
2026-03-16 18:13:40 +0100

    creato file esercizio1.py e scritto docstring

---
+++ Lezione3/esercizio1.py
@@ -0,0 +1,15 @@
+"""
+    somma degli elementi di una lista
+
+    Args: 
+            
+            lista
+    
+    variables:
+
+            somma = contiene somma dei elementi della lista
+    
+    Return:
+            
+            int or float: somma 
+"""
