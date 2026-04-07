e359de31ddd6850d2babd120f7396284ef876da0
SheverinM <majid@pc.home>
2026-04-07 12:08:02 +0200

    modificato testo dell'esercizio

--- Lezione3/esercizio3.py
+++ Lezione3/esercizio3.py
@@ -1,9 +1,8 @@
 
+""" Definire una funzione che prende in input una lista A, indici i e j e scambia il valore di A[i] con A[j]"""
 
 def scambia_elementi_lista(A, i, j):
     """
-        Definire una funzione che prende in input una lista A, indici i e j e scambia il valore di A[i] con A[j]
-
         Args: A: lista.
                    i, j: due indici della lista A
         
