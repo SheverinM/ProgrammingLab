c810e93e5bbf2c9038320e6e1f24b69b888431c0
SheverinM <majid@pc.home>
2026-04-07 10:51:50 +0200

    aggiunto docstring della funzione

--- Lezione3/esercizio3.py
+++ Lezione3/esercizio3.py
@@ -1,1 +1,14 @@
-"""Definire una funzione che prende in input una lista A, indici i e j e scambia il valore di A[i] con A[j]"""
+
+
+def scambia_elementi_lista(A, i, j):
+    """
+        Definire una funzione che prende in input una lista A, indici i e j e scambia il valore di A[i] con A[j]
+
+        Args: A: lista.
+                   i, j: due indici della lista A
+                   
+        Return: Lista con elementi scambiati i <--> j
+        
+        """
+    
+    
