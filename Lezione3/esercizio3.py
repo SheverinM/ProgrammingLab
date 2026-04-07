fee5ca9d8dea98ba33eaae5841c6aaf020ca0805
SheverinM <majid@pc.home>
2026-04-07 12:00:50 +0200

    modificato Docsting

--- Lezione3/esercizio3.py
+++ Lezione3/esercizio3.py
@@ -6,9 +6,18 @@ def scambia_elementi_lista(A, i, j):
 
         Args: A: lista.
                    i, j: due indici della lista A
-                   
-        Return: Lista con elementi scambiati i <--> j
         
         """
-    
-    
+
+    elemento_i_esimo = A[i]
+    elemento_j_esimo = A[j]
+ 
+    A[i] = elemento_j_esimo
+    A[j] = elemento_i_esimo
+
+
+lista = ["Maria", "Majid", "Zagros", "Trilli"]
+
+scambia_elementi_lista(lista, 0, 3)
+
+print(lista)
