40fc99d12e6f3686a19cb3c3da94c87084da4dab
SheverinM <majid@pc.home>
2026-04-09 09:19:44 +0200

    fatto lista vocali

--- Lezione2/esercizio9.py
+++ Lezione2/esercizio9.py
@@ -2,23 +2,8 @@
 
 def vocali_stringa (str):
 
-    """La funzione vocali_stringa conta quante vocali sono presenti in una stringa
-    str è la stinga """
-
-    str_min = str.lower() 
-
     contatore = 0
 
-    for i in str_min:
+    vocale = ["a","e","i","o","u"]
 
-        if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
-
-            contatore += 1
     
-    return contatore
-
-
-str_input = input("Inserisci una stringa e ti dico quante vocali contiene: ")
-
-print(f"ci sono {vocali_stringa(str_input)} vocali in \"{str_input}\"")
-
