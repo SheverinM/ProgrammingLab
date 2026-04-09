50473a5101304dc2fe05fa401cfcb8ef8a0905f6
SheverinM <majid@pc.home>
2026-04-09 09:53:26 +0200

    utilizzato stringa invece di lista

--- Lezione2/esercizio9.py
+++ Lezione2/esercizio9.py
@@ -1,9 +1,17 @@
 #Definire una funzione che conta quante vocali sono presenti in una stringa.
 
-def vocali_stringa (str):
+def vocali_stringa(str):
 
     contatore = 0
 
-    vocale = ["a","e","i","o","u"]
+    #vocale = ["a","e","i","o","u"]
 
+    vocale = "aeiou"
     
+    for lettera in str:
+        if lettera.lower() in vocale:
+            contatore += 1
+    
+    return contatore
+
+print(vocali_stringa("CiAo MONDo"))
